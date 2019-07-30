94e6fbdbe87cffe2c22ae3ae96d857757ebf32e6
Moncef Gaha <moncefplastin07@gmail.com>
2019-07-30 18:23:53 +0100

    تعديلات طفيفة واظافة الدولا الاساسية في منطقة الراس في وورد برس

--- header.php
+++ header.php
@@ -1,1 +1,17 @@
-header.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head();?>
+</head>
+<body>
+<div class="header">
+	<nav class="nav top-nav justify-content-center">
+   	    <a class="nav-link fa fa-facebook" href="#"></a>
+   	    <a class="nav-link fa fa-twitter" href="#"></a>
+   	    <a class="nav-link fa fa-instagram" href="#"></a>
+   	    <a class="nav-link fa fa-rss" href="#"></a>
+   	</nav>
+   	<div class="header-content"></div>
+</div>
