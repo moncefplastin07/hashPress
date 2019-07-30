0776015993b55990bd0b347ab4db69b56b5b24d5
Moncef Gaha <moncefplastin07@gmail.com>
2019-07-30 18:17:43 +0100

    The Basic Points like div warps and files including

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
 get_header();
 ?>
-<-- Main -->
+<!--Main-->
+<div class="body-warp container">
+	<div class="posts-warp row">
+		<!--main-->
+		<div class="post-main col-sm-4">
+			main
+		</div>
+		<!--/main-->
 
-</-- Main -->
+		<!--sidebar-->
+		<div class="sidebar col-sm-4">
+			side bar
+		</div>
+		<!--/sidebar-->
+	</div>
+</div>
+
+<!--/Main-->
 <?php
 get_footer();
 ?>
